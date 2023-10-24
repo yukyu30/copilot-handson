a41f61edf950a9d97b6246b93449fb44e3f70035
Yuki Hattori <yuhattor@users.noreply.github.com>
2023-10-24 11:51:54 +0900

    Update close and done link

---
+++ overwrite.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById("arrow-back").setAttribute("href", "https://yuhattor.github.io/copilot-handson/");
+  document.getElementById("done").setAttribute("href", "https://yuhattor.github.io/copilot-handson/");
+});
